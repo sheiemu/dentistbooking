20feb454663f2208ed149802efaf909c1d23002f
sheiemu <maimunaemu802@gmail.com>
2025-08-23 05:37:42 -0800

    Minor change

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -24,7 +24,7 @@ const doctorTimeSlots = {
     "10:00 AM", "12:00 PM", "2:00 PM"
   ],
   "Dr. Tanjim Noor": [
-    "9:00 AM", "11:00 AM", "1:30 PM", "5:00 PM"
+    "9:00 AM", "11:00 AM", "1:30 PM", "5:00 PM", "8:00 PM"
   ]
 };
 
